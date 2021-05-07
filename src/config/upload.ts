a695143aa7a11066b174bad30b073e9e050cd999
Angel Fernandes <angelfernandes07@hotmail.com>
2021-05-07 14:34:27 -0300

    refactoring upload

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -22,7 +22,7 @@ interface IUploadConfig {
 export default {
   driver: process.env.STORAGE_DRIVER,
   tmpFolder,
-  uploadsFolder: path.resolve(tmpFolder, 'uploads'),
+  uploadsFolder: path.resolve(tmpFolder),
 
   multer: {
     storage: multer.diskStorage({
--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -16,26 +16,28 @@ const app = express();
 app.use(rateLimiter);
 app.use(cors());
 app.use(express.json());
-app.use('/files', express.static(uploadConfig.uploadsFolder));
+app.use('/files', express.static(`${uploadConfig.uploadsFolder}/uploads`));
 app.use(routes);
 
 app.use(errors());
 
-app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
-  if (err instanceof AppError) {
-    return response.status(err.statusCode).json({
+app.use(
+  (err: Request, request: Request, response: Response, _: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        status: 'error',
+        message: err.message,
+      });
+    }
+
+    console.log('caindo aqui', err);
+
+    return response.status(500).json({
       status: 'error',
-      message: err.message,
+      message: 'Internal Server Error',
     });
-  }
-
-  console.error(err);
-
-  return response.status(500).json({
-    status: 'error',
-    message: 'Internal Server Error',
-  });
-});
+  },
+);
 
 app.listen(3333, () => {
   console.log('Server started on port 3333');
